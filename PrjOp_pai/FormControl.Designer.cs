bda4de874c6246679ed132ac76cf0712fc2c9097
amamiex <amamiex@gmail.com>
2016-09-16 20:11:11 +0900

    こみこみ

--- PrjOp_pai/FormControl.Designer.cs
+++ PrjOp_pai/FormControl.Designer.cs
@@ -157,7 +157,7 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Location = new System.Drawing.Point(3, 11);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(40, 16);
             this.label1.TabIndex = 6;
